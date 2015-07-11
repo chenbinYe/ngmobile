9cfc2925222e2d1a3a865c707f1b2db46d586d79
chenbin3206@163.com <chenbin3206@163.com>
2015-07-12 04:54:55 +0800

    test for git

--- src/mod/blog/index/ctrl/index.js
+++ src/mod/blog/index/ctrl/index.js
@@ -2,11 +2,10 @@ define(function (require, exports, module) {
     module.exports = function (app) {
         app.controller(app.cname, ['$scope', '$routeParams', 'RQ', '$timeout', '$location', function ($scope, $routeParams, RQ, $timeout, $location) {
             $scope.test = '你好'
-            console.log('123')
+            //console.log('123')
             RQ.get('test',{},function(res){
                 console.log(res)
-                alert('me')
-                what a pity;asdlkfjaslkdjflkdsajflkasdjflk;asjdflkajsdf;lkj
+                $scope.content = res
             })
         }])
     }
